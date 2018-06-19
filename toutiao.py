3316bdc6eb408c5a1f5c2c34955400c750ef318e
BobcatsII <286577399@qq.com>
2018-06-19 14:50:55 +0800

    Create toutiao.py
    爬取今日头条的街拍图片

---
+++ toutiao.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time   : 2018/6/19 13:37
+# @Author : linan
+# @File   : toutiao.py
+
+
+import os
+import requests
+from urllib.parse import urlencode          #拼接请求参数
+from hashlib import md5                     #以图片内容的MD5值来命名，防止重复命名
+from multiprocessing.pool import Pool       #多线程线程池
+ 
+group_start = 6
+group_end = 8
+
+
+def get_page(offset):
+    params = {
+        'offset': offset,
+        'format': 'json',
+        'keyword': '街拍',
+        'autoload': 'true',
+        'count': '20',
+        'cur_tab': '1',
+        'from': 'search_tab',
+    }
+    url = 'https://www.toutiao.com/search_content/?' + urlencode(params)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+    except requests.ConnectionError:
+        return None
+
+
+def get_images(json):
+    data = json.get('data')
+    if data:
+        for item in data:
+            # print(item)
+            image_list = item.get('image_list')
+            title = item.get('title')
+            # print(image_list)
+            if image_list:
+                for image in image_list:
+                    yield {
+                        'image': image.get('url'),
+                        'title': title
+                    }
+
+
+def save_image(item):
+    if not os.path.exists(item.get('title')):
+        os.mkdir(item.get('title'))
+    try:
+        local_image_url = item.get('image')
+        print (local_image_url)
+        new_image_url = local_image_url.replace('list','large')
+        print (new_image_url)
+        response = requests.get('http:' + new_image_url)
+        if response.status_code == 200:
+            file_path = '{0}/{1}.{2}'.format(item.get('title'), md5(response.content).hexdigest(), 'jpg')
+            if not os.path.exists(file_path):
+                with open(file_path, 'wb')as f:
+                    f.write(response.content)
+            else:
+                print('Already Downloaded', file_path)
+    except requests.ConnectionError:
+        print('Failed to save image')
+
+
+def main(offset):
+    json = get_page(offset)
+    for item in get_images(json):
+        save_image(item)
+
+
+if __name__ == '__main__':
+    pool = Pool()
+    groups = ([x * 20 for x in range(group_start, group_end + 1)])
+    pool.map(main, groups)
+    pool.close()
+    pool.join()
