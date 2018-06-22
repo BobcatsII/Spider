64f11feed0308ddfbc28e955d6484e132576b0f6
BobcatsII <286577399@qq.com>
2018-06-22 17:40:40 +0800

    执行 JavaScript
    selenium API 没有提供下拉进度条的操作，使用execute_script()方法可以实现
    基本上API没有提供的所有功能都可以用执行JavaScript的方式来实现。‘’

--- selenium/execute_javascript.py
+++ selenium/execute_javascript.py
@@ -5,3 +5,4 @@ browser = webdriver.Chrome(r"C:\Program Files (x86)\Google\Chrome\Application\ch
 browser.get("https://www.zhihu.com/explore")
 browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')  #将进度条拉到最下面
 browser.execute_script('alert("To Bottom")')                              #弹出警告提示框
+
