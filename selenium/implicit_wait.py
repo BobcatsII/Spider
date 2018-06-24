6881c17426236805cabdef5240a9f19cead3de53
BobcatsII <286577399@qq.com>
2018-06-24 14:37:10 +0800

    Update implicit_wait.py

--- selenium/implicit_wait.py
+++ selenium/implicit_wait.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+"""
+  用implicitly_wait()方法实现隐式等待
+"""
 
 browser = webdriver.Chrome(r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
 browser.implicitly_wait(10)       #隐式等待
