73d48045e02577bf74a9824598f1452bb730e0d6
BobcatsII <286577399@qq.com>
2018-06-27 11:57:22 +0800

    Create login_simple.py

---
+++ analog/login_simple.py
@@ -0,0 +1,29 @@
+"""
+  简单的模拟登陆
+"""
+
+import requests
+from lxml import html
+
+login_url = "https://github.com/login"
+session_url = "https://github.com/session"
+
+req = requests.session()
+r = req.get(login_url)
+tree = html.fromstring(r.text)
+cok = tree.xpath('//input[@name="authenticity_token"]')[0]
+authenticity_token = cok.attrib["value"]
+
+data = {
+    "commit": "Sign in",
+    "utf8": "✓",
+    "authenticity_token": authenticity_token,
+    "login": "286577399@qq.com",
+    "password": "Ln900318linan"
+}
+
+resp = req.post(session_url, data=data)
+result = html.fromstring(resp.text)
+items_lst = result.xpath('//ul/li/div[contains(@class, "width-full")]/a/span[2]/text()')
+items = ','.join(items_lst)
+print (items)
